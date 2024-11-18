504f36de73bbbbd30e7a9c79d91375e03d0e2c09
selim <91083639+selimAP@users.noreply.github.com>
2024-11-18 10:42:04 +0100

    Centralize head, header, and footer with dynamic JS injection

--- layout.js
+++ layout.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Head HTML
+    const headHTML = `
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../template/card-template.css">
+        <link rel="icon" href="../../../images/Logos/Cozy-logo-small-transparent.png">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    `;
+    document.head.insertAdjacentHTML("beforeend", headHTML);
+
     // Header HTML
     const headerHTML = `
         <header class="header">
@@ -6,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <ul class="navmenu">
                     <div class="logo">
                         <a href="#" class="navlogo">
-                            <img src="/images/Logos/Cozy-logo-transparent-landscape.png" alt="">
+                            <img src="/images/Logos/Cozy-logo-transparent-landscape.png" alt="Logo">
                         </a>
                     </div>
                     <li class="navitem">
@@ -30,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <footer>
             <div class="main-footer">
                 <div class="footer-logo">
-                    <img src="../../images/Logos/Cozy-logo-transparent-landscape.png" alt="">
+                    <img src="../../images/Logos/Cozy-logo-transparent-landscape.png" alt="Logo">
                 </div>
                 <p>
                     <a href="#">GitHub</a> | <a href="#">Contribute</a> | <a href="#">Contact</a>
@@ -42,11 +55,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 </p>
             </div>
         </footer>
-
-        <script src="../../template/card-template.js"></script>
-
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     `;
     document.querySelector("#footer").innerHTML = footerHTML;
+
+    // Footer Scripts
+    const footerScripts = `
+        <script src="../../template/card-template.js"></script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    `;
+    document.body.insertAdjacentHTML("beforeend", footerScripts);
 });
